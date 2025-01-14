8ab051d763e74df052b0211498d117cf5bc5c53a
Lucas Silva <lucas.silva@youxgroup.com.br>
2025-01-14 16:42:34 -0300

    core

--- MATC84TestesSeuBarriga/src/main/java/core/DriverFactory.java
+++ MATC84TestesSeuBarriga/src/main/java/core/DriverFactory.java
@@ -1,5 +1,43 @@
 package core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class DriverFactory {
+	private DriverFactory() {}
+
+	//private static WebDriver driver;
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
+		@Override
+		protected synchronized WebDriver initialValue() {
+			return initDriver();
+		}
+	};
+	
+	public static WebDriver getDriver() {
+		return threadDriver.get();
+	}
+	
+	public static WebDriver initDriver() {
+		WebDriver driver = null;			
+		switch (Properties.browser) {
+			case FIREFOX: driver = new FirefoxDriver();break;
+			case CHROME: driver = new ChromeDriver();break;
+		}
+			driver.manage().window().minimize();
+		return driver;
+	}
+	
+	public static void killDriver() {
+		WebDriver driver = getDriver();
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+		if(threadDriver != null) {
+			threadDriver.remove();
+		}
+	}
 
 }
--- MATC84TestesSeuBarriga/src/main/java/core/Properties.java
+++ MATC84TestesSeuBarriga/src/main/java/core/Properties.java
@@ -1,5 +1,14 @@
 package core;
 
 public class Properties {
+	
+	public static boolean FECHAR_BROWSER = true;
+	
+	public static Browsers browser = Browsers.FIREFOX;
+	
+	public enum Browsers {
+		CHROME,
+		FIREFOX
+	}
 
 }
